4799e8518b4eb70b12286e27936eb54081810b39
Lance Ivy <lance@cainlevy.net>
2020-06-01 14:00:42 -0700

    expose admin endpoints
    i chose axios because it's already a sub-dependency of jwks-rsa

--- src/Client.ts
+++ src/Client.ts
@@ -1,5 +1,6 @@
 import TokenVerifier, { VerifyToken } from "./TokenVerifier";
 import Keychain from "./Keychain";
+import axios, { AxiosRequestConfig } from "axios";
 
 interface ClientConfig {
   // the base url of the service handling authentication
@@ -23,9 +24,22 @@ interface ClientConfig {
   keychainTTL?: number;
 }
 
+interface Account {
+  id: number;
+  username: string;
+  locked: boolean;
+  deleted: boolean;
+}
+
+interface Response<T> {
+  result: T;
+  errors: { field: string; message: string }[];
+}
+
 class Client {
   private config: Required<ClientConfig>;
   private verifier: VerifyToken;
+  private axiosConfig: AxiosRequestConfig;
 
   constructor(config: ClientConfig) {
     const audiences = Array.isArray(config.audiences)
@@ -46,11 +60,68 @@ class Client {
         keychainTTL: this.config.keychainTTL,
       }),
     });
+
+    this.axiosConfig = {
+      auth: {
+        username: config.username,
+        password: config.password,
+      },
+    };
+  }
+
+  async account(id: number): Promise<Account> {
+    return (
+      await axios.get<Response<Account>>(this.accountURL(id), this.axiosConfig)
+    ).data.result;
+  }
+
+  async updateAccount(id: number, data: { username: string }): Promise<void> {
+    await axios.patch(this.accountURL(id), data, this.axiosConfig);
+  }
+
+  async archiveAccount(id: number): Promise<void> {
+    await axios.delete(this.accountURL(id), this.axiosConfig);
+  }
+
+  async lockAccount(id: number): Promise<void> {
+    await axios.patch(this.accountURL(id, "lock"), {}, this.axiosConfig);
+  }
+
+  async unlockAccount(id: number): Promise<void> {
+    await axios.patch(this.accountURL(id, "unlock"), {}, this.axiosConfig);
+  }
+
+  async importAccount(data: {
+    username: string;
+    password: string;
+    locked?: boolean;
+  }): Promise<{ id: number }> {
+    return (
+      await axios.post<Response<{ id: number }>>(
+        `${this.config.adminURL}/accounts/import`,
+        data,
+        this.axiosConfig
+      )
+    ).data.result;
+  }
+
+  async expirePassword(id: number): Promise<void> {
+    await axios.patch(
+      this.accountURL(id, "expire_password"),
+      {},
+      this.axiosConfig
+    );
   }
 
   async subjectFrom(idToken: string | undefined): Promise<string | undefined> {
     if (!idToken) return;
     return (await this.verifier(idToken))["sub"];
+  }
+
+  private accountURL(id: number, action?: string): string {
+    return `${this.config.adminURL}/accounts/${id}${
+      action ? `/${action}` : ""
+    }`;
   }
 }
 
